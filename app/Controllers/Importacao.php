8ec61e2974eeb437a7add5de448d69bf10ae4b67
fmassakir <fmhagihara@yahoo.com.br>
2024-01-22 23:26:55 -0300

    correcoes

--- app/Controllers/Importacao.php
+++ app/Controllers/Importacao.php
@@ -50,7 +50,6 @@ class Importacao extends BaseController
         // Agora $associativeData contém os dados do arquivo XLSX em formato de array associativo
         echo '<pre>';
         print_r($associativeData);
-
     }
 
 
@@ -72,18 +71,18 @@ class Importacao extends BaseController
             $ponteiro = fopen($nomearquivo, 'r');
             $primeiralinha = fgets($ponteiro);
 
-            $primeiralinha = str_replace(array('/',' ', "'", "\r\n"), '', iconv("UTF-8", "ASCII//TRANSLIT", $primeiralinha));
+            $primeiralinha = str_replace(array('/', ' ', "'", "\r\n"), '', iconv("UTF-8", "ASCII//TRANSLIT", $primeiralinha));
 
             $nomesCampos = explode(';', strtolower($primeiralinha));
 
             $lista = array();
             while (!feof($ponteiro)) {
 
-                $registro = array('competencia'=>$competencia, 'arquivo'=>$nomearquivo);
+                $registro = array('competencia' => $competencia, 'arquivo' => $nomearquivo);
                 $linha = fgets($ponteiro);
                 if ($linha) {
                     $valoresCampos = explode(';', $linha);
-                    foreach ($nomesCampos as $key=>$nc) {
+                    foreach ($nomesCampos as $key => $nc) {
                         $registro[$nc] = $valoresCampos[$key];
                     }
                     $lista[] = $registro;
@@ -93,33 +92,32 @@ class Importacao extends BaseController
             $tam_lista = count($lista);
             if ($tam_lista) {
                 $reg_inseridos = 0;
-                foreach($lista as $li) {
+                foreach ($lista as $li) {
                     $inserido = $model->insert($li);
 
-                    if ($inserido) $reg_inseridos ++;
+                    if ($inserido) $reg_inseridos++;
                 }
             }
             echo "Lista: $tam_lista itens. Registros: $reg_inseridos inseridos.";
-
-        }
-        else {
+        } else {
             echo 'Formato n&atilde;o permitido';
         }
     }
 
-    function listar_agrupado($mes=null) {
+    function listar_agrupado($mes = null)
+    {
         if ($mes) {
             $model = new ImportacaoModel();
             $agrupado = $model->agrupar($mes);
             $body_data['agrupado'] = $agrupado;
             $body_data['mes'] = $mes;
             return view('importacao/listar_agrupado', $body_data);
-
         }
     }
 
 
-    function centro_custo($mes=null) {
+    function centro_custo($mes = null)
+    {
         if ($mes) {
             $model = new ImportacaoModel();
             $agrupado = $model->agruparCentroCusto($mes);
@@ -132,27 +130,33 @@ class Importacao extends BaseController
             }
             */
             var_dump($agrupado);
-           // $body_data['agrupado'] = $agrupado;
-           // return view('importacao/listar_agrupado', $body_data);
+            // $body_data['agrupado'] = $agrupado;
+            // return view('importacao/listar_agrupado', $body_data);
 
         }
     }
 
 
-    function gerar_xml($mes=null) {
+    function gerar_xml($mes = null)
+    {
         if ($mes) {
+
+            // Dados do BD
             $model = new ImportacaoModel();
             $agrupado = $model->agruparCentroCusto($mes);
+
+
+            // Variáveis para o Header
             $mesano = substr($mes, 5, 2) . substr($mes, 0, 4);
             $datacredito = date('tmY', strtotime($mes));
             $datahorageracao = date('dmYHi');
+
+            // Header do XML
             $xml = '<Implanta>' . PHP_EOL .
-            '  <Header NumeroLote="177" MesAno="' . $mesano . '" DataCreditoContabil="' . $datacredito .
-            '" DataHoraGeracao="' . $datahorageracao . '" CNPJ="76639384000159" UF="PR" Versao="1.0" TipoArquivo="FolhaPagamento" SistemaOrigem="BENNER" SistemaDestino="SISCONT.NET" />' . PHP_EOL;
+                '  <Header NumeroLote="177" MesAno="' . $mesano . '" DataCreditoContabil="' . $datacredito .
+                '" DataHoraGeracao="' . $datahorageracao . '" CNPJ="76639384000159" UF="PR" Versao="1.0" TipoArquivo="FolhaPagamento" SistemaOrigem="BENNER" SistemaDestino="SISCONT.NET" />' . PHP_EOL;
 
-            $tipoAnterior = 'llll';
-            $codVerbaAnterior = 0;
-
+            // Variáveis para o miolo do XML
             $valorDesconto = array();
             $histDesconto = array();
             $contaDesconto = array();
@@ -160,19 +164,9 @@ class Importacao extends BaseController
 
             $xml_BlocoA = $xml_BlocoC = $xml_BlocoF = '';
 
+
             foreach ($agrupado as $ag) {
-                $centrodecustof = substr($ag['centrodecusto'], 0, 1) . '.' . substr($ag['centrodecusto'], 1, 2) . '.' . substr($ag['centrodecusto'], 3);
-
-
-                if ($ag['tipo'] && $tipoAnterior && $ag['tipo'] != $tipoAnterior) {
-                    if ($tipoAnterior === 'A - Despesa') $xml .= '  </BlocoA>' . PHP_EOL;
-                    if ($tipoAnterior === 'C - Desconto') $xml .= '  </BlocoC>' . PHP_EOL;
-                    if ($tipoAnterior === 'F - Estorno') $xml .= '  </BlocoF>' . PHP_EOL;
-                    if ($ag['tipo'] === 'A - Despesa') $xml .= '  <BlocoA>' . PHP_EOL;
-                    if ($ag['tipo'] === 'C - Desconto') $xml .= '  <BlocoC>' . PHP_EOL;
-                    if ($ag['tipo'] === 'F - Estorno') $xml .= '  <BlocoF>' . PHP_EOL;
-                    $qtdeBlocos++;
-                }
+                $centrodecustof = substr($ag['centrodecusto'], 0, 1) . '.' . substr($ag['centrodecusto'], 1, 3) . '.' . substr($ag['centrodecusto'], 4, 2);
 
                 if ($ag['tipo'] === 'A - Despesa') {
 
@@ -187,7 +181,7 @@ class Importacao extends BaseController
                 }
                 if ($ag['tipo'] === 'C - Desconto') {
                     $cv = $ag['id_grupo'];
-                    if (!isset($valorDesconto[$cv])) $valorDesconto[$cv] = $histDesconto[$cv]= $contaDesconto[$cv] = 0;
+                    if (!isset($valorDesconto[$cv])) $valorDesconto[$cv] = $histDesconto[$cv] = $contaDesconto[$cv] = 0;
                     $valorDesconto[$cv] += $ag['soma'];
                     $histDesconto[$cv] = $ag['nome_grupo'];
                     $contaDesconto[$cv] = $ag['conta_banco'];
@@ -205,39 +199,72 @@ class Importacao extends BaseController
                     $xml_BlocoF .= '    </Estorno>' . PHP_EOL;
                     $qtdeRegistros++;
                 }
-
-
-                $tipoAnterior = $ag['tipo'];
             }
-            if (!empty($valorDesconto)) {
-                $qtdeRegistros += count($valorDesconto);
 
-                foreach ($valorDesconto as $key=>$value) {
+            // Grupos do bloco C
+            if (!empty($valorDesconto)) {
+
+                foreach ($valorDesconto as $key => $value) {
                     $xml_BlocoC .= '    <Desconto>' . PHP_EOL;
                     $xml_BlocoC .= '      <Valor>' . number_format($value, 2, '', '') . '</Valor>' . PHP_EOL;
                     $xml_BlocoC .= '      <Historico>' . $histDesconto[$key] . '</Historico>' . PHP_EOL;
                     $xml_BlocoC .= '      <CodigoConta>' . $contaDesconto[$key] . '</CodigoConta>' . PHP_EOL;
                     $xml_BlocoC .= '    </Desconto>' . PHP_EOL;
+                    $qtdeRegistros++;
                 }
                 $valorDesconto = [];
-
             }
-            if ($tipoAnterior === 'A - Despesa') $xml .= '  </BlocoA>' . PHP_EOL;
-            if ($tipoAnterior === 'F - Estorno') $xml .= '  </BlocoF>' . PHP_EOL;
 
+            // Para cada bloco, se tiver registros, inclui no XML principal
+            if ($xml_BlocoA) {
+                $qtdeBlocos++;
+                $xml .= '  <BlocoA>' . PHP_EOL . $xml_BlocoA . '  </BlocoA>' . PHP_EOL;
+            }
 
+            if ($xml_BlocoC) {
+                $qtdeBlocos++;
+                $xml .= '  <BlocoC>' . PHP_EOL . $xml_BlocoC . '  </BlocoC>' . PHP_EOL;
+            }
 
+            if ($xml_BlocoF) {
+                $qtdeBlocos++;
+                $xml .= '  <BlocoF>' . PHP_EOL . $xml_BlocoF . '  </BlocoF>' . PHP_EOL;
+            }
+
+            // Trailler do XML
             $xml .= '  <Trailer QuantidadeBlocos="' . $qtdeBlocos . '" QuantidadeTotalRegistros="' . $qtdeRegistros . '" />' . PHP_EOL .
-            '</Implanta>';
+                '</Implanta>';
 
 
-            echo '<textarea cols="300" rows="50">' . $xml . '</textarea>';
+            // Nome do arquivo e caminho onde o arquivo XML será salvo temporariamente
+            $nome_arquivo = "implanta.net.folha.$mesano.pr.xml";
+            $caminho_arquivo = sys_get_temp_dir() . '/' . $nome_arquivo;
 
+            // Criação do arquivo XML
+            $file = fopen($caminho_arquivo, 'w');
+            fwrite($file, $xml);
+            fclose($file);
+
+            // Define os cabeçalhos para forçar o download do arquivo
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename=' . $nome_arquivo);
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($caminho_arquivo));
+
+            // Envia o arquivo para o cliente
+            readfile($caminho_arquivo);
+
+            // Remove o arquivo temporário após o download
+            unlink($caminho_arquivo);
         }
     }
 
 
-    function agrupar_descontos() {
+    function agrupar_descontos()
+    {
         $arquivo = $_FILES['arquivo']['tmp_name'];
         $nomearquivo = $_FILES['arquivo']['name'];
 
@@ -247,7 +274,7 @@ class Importacao extends BaseController
         $blocoC_fim = strpos($conteudo, '</BlocoC>');
 
         // Pega somente a parte que está dentro do bloco C
-        $blocoC = substr($conteudo, $blocoC_inicio, $blocoC_fim-$blocoC_inicio+9);
+        $blocoC = substr($conteudo, $blocoC_inicio, $blocoC_fim - $blocoC_inicio + 9);
 
         // Convert xml string into an object
         $new = simplexml_load_string(trim($blocoC));
@@ -269,15 +296,14 @@ class Importacao extends BaseController
             if ($resumido != $anterior) {
                 $verba[$resumido] = $valor;
                 $hverba[$resumido] = $historico;
-            }
-            else {
+            } else {
                 $verba[$resumido] += $valor;
             }
             $anterior = $resumido;
         }
 
         $novoBlocoC = '<BlocoC>';
-        foreach ($verba as $cod=>$val) {
+        foreach ($verba as $cod => $val) {
             $novoBlocoC .= '
     <Desconto>
       <Valor>' . $val . '</Valor>
@@ -289,12 +315,12 @@ class Importacao extends BaseController
   </BlocoC>';
 
         $pos_qtd_registros = strpos($conteudo, 'QuantidadeTotalRegistros');
-        $filtrar = substr($conteudo, $pos_qtd_registros+26, 6);
-        $qtde = preg_replace("/\D/","", $filtrar);
+        $filtrar = substr($conteudo, $pos_qtd_registros + 26, 6);
+        $qtde = preg_replace("/\D/", "", $filtrar);
 
         $novaQtde = (int) $qtde - count($descontos) + count($verba);
 
-        $novoconteudo = substr($conteudo, 0, $blocoC_inicio) . $novoBlocoC . substr($conteudo, $blocoC_fim+9);
+        $novoconteudo = substr($conteudo, 0, $blocoC_inicio) . $novoBlocoC . substr($conteudo, $blocoC_fim + 9);
 
         $antesRegistro = 'QuantidadeTotalRegistros="' . $qtde . '"';
         $depoisRegistro = 'QuantidadeTotalRegistros="' . $novaQtde . '"';
@@ -306,21 +332,20 @@ class Importacao extends BaseController
 
         sleep(2);
         file_put_contents('modificado.xml', $novoconteudo);
-
-
     }
 
-    function array_to_xml( $data, &$xml_data ) {
-        foreach( $data as $key => $value ) {
-            if( is_array($value) ) {
-                if( is_numeric($key) ){
-                    $key = 'item'.$key; //dealing with <0/>..<n/> issues
+    function array_to_xml($data, &$xml_data)
+    {
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                if (is_numeric($key)) {
+                    $key = 'item' . $key; //dealing with <0/>..<n/> issues
                 }
                 $subnode = $xml_data->addChild($key);
                 $this->array_to_xml($value, $subnode);
             } else {
-                $xml_data->addChild("$key",htmlspecialchars("$value"));
+                $xml_data->addChild("$key", htmlspecialchars("$value"));
             }
-         }
+        }
     }
 }
