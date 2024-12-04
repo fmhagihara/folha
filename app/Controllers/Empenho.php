a840d25bc8ef2f908410d1eb7e0be70607bc2188
Massaki <fmhagihara@yahoo.com.br>
2024-12-04 14:39:57 -0300

    Novo Webservice para Reserva Orçamentária - Empenho

--- app/Controllers/Empenho.php
+++ app/Controllers/Empenho.php
@@ -23,6 +23,117 @@ class Empenho extends Controller
       // Obtém a data formatada como string
       $ultimoDiaDoMes = $date->format('Y-m-d');
 
+      foreach ($agrupado as $ag) {
+         if ($ag['id_grupo'] == $idgrupo) {
+            $ccusto = substr($ag['centrodecusto'], 0, 4);
+            $empenho['conta'] = $ag['conta_empenho'];
+            $empenho['nomeconta'] = $ag['nome_grupo'];
+            if (isset($empenho['total_empenho'])) $empenho['total_empenho'] += $ag['soma'];
+            else $empenho['total_empenho'] = $ag['soma'];
+            if (isset($subprograma[$ccusto])) $subprograma[$ccusto] += $ag['soma'];
+            else $subprograma[$ccusto] = $ag['soma'];
+         }
+      }
+
+      if (isset($empenho, $subprograma)) {
+         //  var_dump($empenho);
+         //var_dump($subprograma);
+
+
+         // Endpoint
+         $url = "https://crea-pr.implanta.net.br/siscont/servico/WebApi/Despesa/IncluirSolicitacaoReservaOrcamentaria";
+
+         // Subarray que será adicionada ao array de dados
+         foreach ($subprograma as $key => $value) {
+            $distribCC[] = [
+               'CentroCustoCodigo' => substr($key, 0, 1) . '.' . substr($key, 1, 3),
+               'Valor' => (float) $value
+            ];
+         }
+
+         // Array de dados
+         $data = [
+            'ContaContabil' => $empenho['conta'],
+            'FavorecidoNome' => 'Folha Pagamento CREA-PR',
+            'FavorecidoCPFCNPJ' => '76639384000159',
+            'EmpenhoValor' => round($empenho['total_empenho'],2),
+            'EmpenhoData' => $ultimoDiaDoMes,
+            'EmpenhoTipo' => 'Estimativo',
+            'SolicitacaoTipo' => 'Empenho',
+            'NumeroProcesso' => '017.003078/2023-00',
+            'Historico' => 'Despesa com pagamento de verbas salariais em ' . substr($mes, 5, 2) . '/' . substr($mes, 0, 4),
+            'Justificativa' => $empenho['conta'] . ' ' . $empenho['nomeconta'],
+            'DistribuicoesCentroCusto' => $distribCC
+         ];
+
+         try {
+            // Converte os dados para JSON
+            $jsonData = json_encode($data);
+
+            // Inicializa o cURL
+            $ch = curl_init();
+
+            // Configurações do cURL
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Desabilitar verificação SSL do host
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desabilitar verificação SSL do certificado
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+               'Content-Type: application/json',
+               'Content-Length: ' . strlen($jsonData),
+               'chave: b2332fcd-5870-4d40-94c1-0294bae63548',
+               'senha: cbe535e3-a24e-4bb2-81bc-c338f682865a'
+            ]);
+
+            // Executa a requisição
+            $response = curl_exec($ch);
+
+            // Verifique se há erros na requisição cURL
+            if (curl_errno($ch)) {
+               $error_msg = curl_error($ch);
+               throw new \Exception($error_msg);
+            }
+
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            // Fecha o cURL
+            curl_close($ch);
+
+            if ($response && $httpCode == 200) {
+               $resposta = json_decode($response, true);
+               if (isset($resposta['Entity'])) {
+                  echo 'Reserva orçamentária realizada com sucesso! ID ' . $resposta['Entity'];
+               }
+               else {
+                  var_dump($resposta);
+               }
+            }
+
+         } catch (\Exception $e) {
+            // Trate erros genéricos
+            echo "Erro: " . $e->getMessage();
+            log_message('error', 'Erro: ' . $e->getMessage());
+         }
+      }
+   }
+
+
+   // Modo antigo WCF (Despesa.svc)
+   public function enviar_old($mes, $idgrupo)
+   {
+      $model = new ImportacaoModel();
+      $agrupado = $model->agruparCentroCusto($mes);
+
+      $date = new DateTime($mes);
+
+      // Define o dia para o último dia do mês
+      $date->modify('last day of this month');
+
+      // Obtém a data formatada como string
+      $ultimoDiaDoMes = $date->format('Y-m-d');
+
       foreach ($agrupado as $ag) :
          if ($ag['id_grupo'] == $idgrupo) {
             $ccusto = substr($ag['centrodecusto'], 0, 4);
@@ -36,33 +147,33 @@ class Empenho extends Controller
       endforeach;
 
       if (isset($empenho, $subprograma)) {
-       //  var_dump($empenho);
+         //  var_dump($empenho);
          //var_dump($subprograma);
 
 
-      // URL do serviço SOAP
-      $url = "https://crea-pr.implanta.net.br/siscont/servico/Integracao/Despesa.svc/soap1";
+         // URL do serviço SOAP
+         $url = "https://crea-pr.implanta.net.br/siscont/servico/Integracao/Despesa.svc/soap1";
 
-      //$xmlEnvelope = $this->createXmlEnvelope($empenho, $subprograma, $ultimoDiaDoMes, $mes);
+         //$xmlEnvelope = $this->createXmlEnvelope($empenho, $subprograma, $ultimoDiaDoMes, $mes);
 
 
-      // Envelope XML customizado
-      $xmlEnvelope =
-'<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/" xmlns:imp="http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity">
+         // Envelope XML customizado
+         $xmlEnvelope =
+            '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/" xmlns:imp="http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity">
    <soapenv:Header/>
    <soapenv:Body>
       <tem:IncluirSolicitacaoReservaOrcamentaria>
          <tem:solicitacao>
             <imp:ContaContabil>' . $empenho['conta'] . '</imp:ContaContabil>
             <imp:DistribuicoesCentroCusto>';
-            foreach ($subprograma as $key=>$value) :
-               $xmlEnvelope .=
+         foreach ($subprograma as $key => $value) :
+            $xmlEnvelope .=
                '<imp:SolicitacoesReservasOrcamentariasCentroCustoEntity>
                   <imp:CentroCustoCodigo>' . substr($key, 0, 1) . '.' . substr($key, 1, 3) . '</imp:CentroCustoCodigo>
                   <imp:Valor>' . $value . '</imp:Valor>
                </imp:SolicitacoesReservasOrcamentariasCentroCustoEntity>';
-            endforeach;
-            $xmlEnvelope .=
+         endforeach;
+         $xmlEnvelope .=
             '</imp:DistribuicoesCentroCusto>
             <imp:EmpenhoData>' . $ultimoDiaDoMes . '</imp:EmpenhoData>
             <imp:EmpenhoTipo>Ordinario</imp:EmpenhoTipo>
@@ -77,9 +188,6 @@ class Empenho extends Controller
       </tem:IncluirSolicitacaoReservaOrcamentaria>
    </soapenv:Body>
 </soapenv:Envelope>';
-
-
-
       }
 
 
@@ -138,36 +246,5 @@ class Empenho extends Controller
          echo "Erro: " . $e->getMessage();
          log_message('error', 'Erro: ' . $e->getMessage());
       }
-
    }
-
-   function createXmlEnvelope($empenho, $subprograma, $ultimoDiaDoMes, $mes) {
-      $xml = new SimpleXMLElement('<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/" xmlns:imp="http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity"></soapenv:Envelope>');
-
-      $xml->addChild('soapenv:Header');
-      $body = $xml->addChild('soapenv:Body');
-      $incluirSolicitacao = $body->addChild('tem:IncluirSolicitacaoReservaOrcamentaria', '', 'http://tempuri.org/');
-      $solicitacao = $incluirSolicitacao->addChild('tem:solicitacao', '', 'http://tempuri.org/');
-
-      $solicitacao->addChild('imp:ContaContabil', htmlspecialchars($empenho['conta']), 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-
-      $distribuicoesCentroCusto = $solicitacao->addChild('imp:DistribuicoesCentroCusto', '', 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      foreach ($subprograma as $key => $value) {
-          $entidade = $distribuicoesCentroCusto->addChild('imp:SolicitacoesReservasOrcamentariasCentroCustoEntity', '', 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-          $entidade->addChild('imp:CentroCustoCodigo', htmlspecialchars(substr($key, 0, 1) . '.' . substr($key, 1, 3)), 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-          $entidade->addChild('imp:Valor', htmlspecialchars($value), 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      }
-
-      $solicitacao->addChild('imp:EmpenhoData', htmlspecialchars($ultimoDiaDoMes), 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      $solicitacao->addChild('imp:EmpenhoTipo', 'Ordinario', 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      $solicitacao->addChild('imp:EmpenhoValor', htmlspecialchars($empenho['total_empenho']), 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      $solicitacao->addChild('imp:FavorecidoCPFCNPJ', '76639384000159', 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      $solicitacao->addChild('imp:FavorecidoNome', 'Folha Pagamento CREA-PR', 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      $solicitacao->addChild('imp:Historico', 'Despesa com pagamento de verbas salariais em ' . htmlspecialchars(substr($mes, 5, 2) . '/' . substr($mes, 0, 4)), 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      $solicitacao->addChild('imp:Justificativa', 'Automático folha', 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      $solicitacao->addChild('imp:NumeroProcesso', '017.003078/2023-00', 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-      $solicitacao->addChild('imp:SolicitacaoTipo', 'Empenho', 'http://schemas.datacontract.org/2004/07/Implanta.Siscont.Business.Service.Integracao.Entity');
-
-      return $xml;
-  }
 }
